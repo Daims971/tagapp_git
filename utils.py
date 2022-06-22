22c22fbe8c81d19b95c8e348f86e03994a21bdd9
Daims971 <frechou.damien@gmail.com>
2022-06-22 16:31:09 +0200

    Passage de variables/paramètres entre index et result + création de fonctions dans utils et utilisation dans views

--- utils.py
+++ utils.py
@@ -1,15 +1,136 @@
 
 # Importation générale
 
+import pandas as pd
+# pd.set_option('display.max_columns', None)
+import numpy as np
 import joblib
+
+# Importation des librairies pour la prédiction
+
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import PowerTransformer
+
+from sklearn.pipeline import Pipeline
+from sklearn import linear_model
+from sklearn.metrics import jaccard_score, make_scorer
+
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+
+import re 
 
 import os
 import sys
 print(os.getcwd(), file=sys.stdout)
 
 
+# -------------- Define scoring functions
+def jaccard_func(y_true, y_pred):
+    result_score = jaccard_score(y_true, y_pred, average='samples',zero_division=0)
+    return result_score
+
+# -------------- Score and error functions
+score_jaccard_func = make_scorer(jaccard_func, greater_is_better=True)
+scoring_model = {'score_jaccard_func':score_jaccard_func, 'accuracy':'accuracy'}
+
 # Import des modèles entrainés
 
 
 estimator = joblib.load(os.getcwd() + '\\tagapp\\tfidf_input_pipe_pac_cv.pkl')
+
+
+
+# ---------------------- Fonction de Nettoyage -------------------
+
+# Tokenizer
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+def tokenizer_fct(sentence) :
+    # remove or replace by empty or space char a specific symbol
+    sentence_clean = sentence.replace('-', ' ').replace('/', ' ').replace('_', ' ').replace('<', ' ') \
+    .replace('>', ' ').replace(':', ' ').replace('=', ' ').replace('.', ' ').replace("'", ' ').replace("\\", ' ')
+    # Ajout de _, <, >, ', :, =, .
+    word_tokens = word_tokenize(sentence_clean)
+    return word_tokens
+
+def digit_clean(list_words):
+    # remove or replace by '' a char word from list_words only composed by digits
+    filtered_w = [re.sub("^[0-9]+[0-9]$", "", w) for w in list_words] 
+    return filtered_w
+    
+def body_html_tag_out(sentence):
+    # remove or replace by empty or space char only < or >
+    filtered_sentence = re.sub("<.+?>", " ", sentence)
+    return filtered_sentence
+    
+def non_latin_char_clean(list_words):
+    # replace all non latin char by empty char
+    filtered_w = [re.sub("([^\x00-\x7F])+", "", w) for w in list_words]
+    return filtered_w
+
+    
+# Stop words
+from nltk.corpus import stopwords
+stop_w = list(set(stopwords.words('english'))) + ['[', ']', ',', '.', ':', '?', '(', ')','','=',"'"]
+
+def stop_word_filter_fct(list_words) :
+    filtered_w = [w for w in list_words if not w in stop_w]
+    filtered_w2 = [w for w in filtered_w if len(w) > 2]
+    return filtered_w2
+
+# lower case et alpha
+def lower_start_fct(list_words) :
+    lw = [w.lower() for w in list_words if (not w.startswith("@")) 
+    #                                   and (not w.startswith("#"))
+                                       and (not w.startswith("http"))]
+    return lw
+
+
+
+# Fonction de préparation du texte pour le bag of words (Countvectorizer et Tf_idf, Word2Vec)
+def transform_bow_fct(desc_text) :
+    word_tokens = tokenizer_fct(desc_text)
+    word_tokens_bis = non_latin_char_clean(word_tokens)
+    lw = lower_start_fct(word_tokens_bis)
+    sw = stop_word_filter_fct(lw)
+    sw_bis = digit_clean(sw)
+#     lw = lower_start_fct(sw_bis)
+    # lem_w = lemma_fct(lw)    
+    transf_desc_text = ' '.join(sw_bis)
+    return transf_desc_text
+
+
+
+# Tokenizer
+import nltk
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+def tokenizer_tags(sentence) :
+    # print(sentence)
+    sentence_clean = sentence.replace('<', ' ').replace('>', ' ').replace('-', ' ').replace(':', ' ')
+    word_tokens = word_tokenize(sentence_clean.lower())
+    word_tokens_bis = non_latin_char_clean(word_tokens)
+    lw = lower_start_fct(word_tokens_bis)
+    sw = stop_word_filter_fct(lw)
+    sw_bis = digit_clean(sw)
+#     lw = lower_start_fct(sw_bis)
+    transf_desc_text = ' '.join(sw_bis)
+    transf_desc_text_2 = transform_bow_fct(transf_desc_text)
+    lw_bis = tokenizer_fct(transf_desc_text_2)
+    word_tokens_sorted = sorted(set(lw_bis))
+    return word_tokens_sorted
+
+
+# ---------------------------- Nettoyage Question & predictions
+
+def pred_tags(user_question):
+    user_question_cleaned = transform_bow_fct(user_question)
+    
+    y_pred = estimator.best_estimator_.fit_transform([user_question_cleaned])
+    
+    
+    return y_pred
 
--- views.py
+++ views.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, url_for, request
 # import config
 import sys
 
+from .utils import pred_tags
 
 app = Flask(__name__)
 
@@ -10,7 +11,7 @@ app = Flask(__name__)
 app.config.from_pyfile('config.py')
 # To get one variable, tape app.config['MY_VARIABLE']
 
-@app.route('/',methods=["POST"])
+@app.route('/',methods=["POST","GET"])
 def index():
     #print('This is standard output', file=sys.stdout)
     # if request.method == 'GET':
@@ -18,9 +19,13 @@ def index():
     if request.method == 'POST':
         form_data = request.form['question'] #request.form('question')
         print('form_data = '+form_data, file=sys.stdout)
-        data_result = form_data
+        data_result = pred_tags(form_data)[0]
+        for res in data_result:
+            print('data_result_i = '+res, file=sys.stdout)
         
-        return render_template('result.html', data_result)
+        myDict = {data_result[i]: data_result[i] for i in range(0, len(data_result), 1)}
+        
+        return render_template('result.html', data_result[0])
     else:
         return render_template('index.html')
     # return "Hello world !"
